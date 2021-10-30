137256deff343013fc13bf26fcb807a76d9c0246
Satvik Sharma <satvik.sharma2@gmail.com>
2021-10-30 20:19:21 +1100

    PoC with separate dockerfiles

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-git show main:package.json > main.package.json
-git show main:yarn.lock > main.yarn.lock
-
 docker build -t app:latest .
-
-rm main.package.json main.yarn.lock
--- scripts/build-base.sh
+++ scripts/build-base.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 git show main:package.json > main.package.json
 git show main:yarn.lock > main.yarn.lock
 
-docker build --target base_build -t app-base:latest .
+docker build -f base.Dockerfile -t app-base:latest .
 
 rm main.package.json main.yarn.lock
